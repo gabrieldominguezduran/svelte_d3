991141be799112d343bfd00caf4076d9e8908a03
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-07-18 10:52:55 +0200

    Add reazctive variables

--- src/components/charts/WorldMap/Map.svelte
+++ src/components/charts/WorldMap/Map.svelte
@@ -4,4 +4,20 @@
   import countryShapes from "./country-shapes";
   import scaleCanvas from "./scale-canvas";
   import { tweened } from "./tweened-staggered";
+
+  const move = (x, y) => `transform: translate(${x}px, ${y}px)`;
+
+  export let data = [];
+
+  export let nameAccessor = (d) => d[0];
+  export let rAccesor = (d) => d[1];
+  export let colorAccessor = (d) => d[2];
+
+  let width = 1200;
+  $: isVertical = width < 450;
+  $: height = isVertical ? 2 : 0.7;
+
+  const sphere = { type: "Sphere" };
+  let canvasElement;
+  let blankMap;
 </script>
