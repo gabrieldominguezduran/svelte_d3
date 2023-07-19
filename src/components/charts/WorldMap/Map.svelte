2d4e5af8bfa2ef334d93ac1a55b95620b6b5a694
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-07-19 16:44:49 +0200

    Add blank canvas world map

--- src/components/charts/WorldMap/Map.svelte
+++ src/components/charts/WorldMap/Map.svelte
@@ -5,19 +5,106 @@
   import scaleCanvas from "./scale-canvas";
   import { tweened } from "./tweened-staggered";
 
+  // utility function for translating elements
   const move = (x, y) => `transform: translate(${x}px, ${y}px)`;
 
   export let data = [];
 
+  // accessor functions to quickly pivot between data structures
   export let nameAccessor = (d) => d[0];
-  export let rAccesor = (d) => d[1];
+  export let rAccessor = (d) => d[1];
   export let colorAccessor = (d) => d[2];
 
   let width = 1200;
   $: isVertical = width < 450;
-  $: height = isVertical ? 2 : 0.7;
+  $: height = width * (isVertical ? 2 : 0.7);
 
   const sphere = { type: "Sphere" };
   let canvasElement;
   let blankMap;
+
+  // create projections - vertical orientations have two globes
+  $: projections = isVertical
+    ? [
+        d3.geoOrthographic().fitSize([width, width], sphere).rotate([-75, -20]),
+        d3
+          .geoOrthographic()
+          .fitSize([width, width], sphere)
+          .translate([width / 2, height * 0.7])
+          .rotate([70, -20]),
+      ]
+    : [d3.geoOrthographic().fitSize([width, height], sphere).rotate([-6, 0])];
+
+  //scales
+  $: rScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(data, rAccessor)])
+    .range([0, width * height * 0.000036])
+    .clamp(true);
+
+  $: colorScale = d3
+    .scaleLinear()
+    .domain([0, d3.extent(data, colorAccessor)])
+    .range(["#274B55", "#f8f8f8"])
+    .interpolate(d3.interpolateHcl)
+    .clamp(true);
+
+  // draw blank map
+  const drawCanvas = () => {
+    if (!canvasElement) return;
+    const ctx = canvasElement.getContext("2d");
+    scaleCanvas(canvasElement, ctx, width, height);
+
+    const drawMap = (projection) => {
+      const path = d3.geoPath(projection, ctx);
+      const drawPath = (shape) => {
+        ctx.beginPath();
+        path(shape);
+      };
+      drawPath(sphere);
+
+      const fill = (color) => {
+        ctx.fillStyle = color;
+        ctx.fill();
+      };
+      const stroke = (color) => {
+        ctx.strokeStyle = color;
+        ctx.stroke();
+      };
+      drawPath(sphere);
+      fill("#fff");
+      stroke("#bbb");
+      drawPath(d3.geoGraticule10());
+      stroke("#eee");
+
+      countryShapes.forEach((shape) => {
+        drawPath(shape);
+        fill("#f8f8f8");
+        stroke("#ccc");
+      });
+
+      drawPath(sphere);
+      stroke("#ccc");
+    };
+    projections.forEach(drawMap);
+
+    blankMap = ctx.getImageData(0, 0, width * 2, height * 2);
+  };
+
+  $: width, projections, drawCanvas();
 </script>
+
+<figure class="canvas-wrapper" bind:clientWidth={width}>
+  <canvas
+    style={`width: ${width}px; height: ${height}px`}
+    bind:this={canvasElement}
+  />
+</figure>
+
+<style>
+  .canvas-wrapper {
+    position: relative;
+    width: 100%;
+    margin: 0;
+  }
+</style>
--- src/components/charts/WorldMap/MapWrapper.svelte
+++ src/components/charts/WorldMap/MapWrapper.svelte
@@ -1,14 +1,36 @@
 <script>
+  import countryShapes from "./country-shapes.js";
   import Map from "./Map.svelte";
+
+  const createData = () => {
+    return countryShapes.map((d) => [
+      d.properties.geounit,
+      Math.random() < 0.13 ? Math.random() : 0,
+      Math.random(),
+    ]);
+  };
+  let data = createData();
 </script>
 
-<main class="chart-container scatter">
+<main class="chart-container">
   <h1>Map</h1>
   <div class="note">CLick to update</div>
-  <Map />
+  <div class="wrapper">
+    <Map
+      {data}
+      nameAccessor={(d) => d[0]}
+      rAccessor={(d) => d[1]}
+      colorAccessor={(d) => d[2]}
+    />
+  </div>
 </main>
 
 <style>
+  .wrapper {
+    background: #f8f8f8;
+    margin: 0;
+    padding: 1rem;
+  }
   .note {
     font-style: italic;
     color: var(--text-light);
