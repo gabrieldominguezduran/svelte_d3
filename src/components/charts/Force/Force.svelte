17d66607ca0020c1e023024467098f4d6a1c6d71
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-08-17 11:28:38 +0200

    Add simulation

--- src/components/charts/Force/Force.svelte
+++ src/components/charts/Force/Force.svelte
@@ -1,3 +1,21 @@
 <script>
   import * as d3 from "d3";
+
+  const move = (x, y) => `transform: translate(${x}px, ${y}px)`;
+
+  export let dots = [];
+  export let forces = [];
+
+  let usedForces = [];
+  let renderDots = [];
+
+  let width = 1200;
+  $: height = width;
+
+  $: simulation = d3
+    .forceSimulation()
+    .nodes(dots)
+    .on("tick", () => {
+      renderDots = [...dots];
+    });
 </script>
