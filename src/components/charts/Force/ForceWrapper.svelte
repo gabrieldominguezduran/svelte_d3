300b7049133165fc494a356418949901b52571c9
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-08-18 11:03:23 +0200

    Add forces to wrapper WIP

--- src/components/charts/Force/ForceWrapper.svelte
+++ src/components/charts/Force/ForceWrapper.svelte
@@ -1,3 +1,30 @@
+<script>
+  import * as d3 from "d3";
+
+  import Force from "./Force.svelte";
+
+  let element;
+
+  let centerPosition = [200, 200];
+  let useForceCollide = true;
+  let useForceRadial = true;
+  $: activeForceX = d3.forceX().x(centerPosition[0]);
+  $: activeForceY = d3.forceY().y(centerPosition[1]);
+  $: activeForceCollide = d3.forceCollide().radius(10).iterations(3);
+  $: activeForceRadial = d3
+    .forceRadial()
+    .radius(150)
+    .x(centerPosition[0])
+    .y(centerPosition[1]);
+
+  $: forces = [
+    ["x", activeForceX],
+    ["y", activeForceY],
+    useForceCollide && ["collide", activeForceCollide],
+    useForceRadial && ["radial", activeForceRadial],
+  ].filter((d) => d);
+</script>
+
 <main class="chart-container">
   <h1>Force</h1>
   <div class="note">Click to update</div>
